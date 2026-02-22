5ff056df4a7b0ebe744a6373ad82f41b732e3e37
Akram HUSSAIN <akram.hussain@alten-india.com>
2026-02-22 13:01:08 +0530

    products added

--- maatikala.py
+++ maatikala.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,
+            template_folder="templates",
+            static_folder="static/static")
+
+# Sample product list (later can connect to database)
+products_data = [
+    {
+        "name": "Terracotta Pot",
+        "price": "₹799",
+        "image": "images\front-view-dolma-along-with-yogurt-inside-brown-pots-grey-floor.jpg"
+    },
+    {
+        "name": "Clay Diya Set",
+        "price": "₹299",
+        "image": "images\front-view-dolma-along-with-yogurt-inside-brown-pots-grey-floor.jpg"
+    },
+    {
+        "name": "Mud Table Lamp",
+        "price": "₹1,499",
+        "image": "images\front-view-dolma-along-with-yogurt-inside-brown-pots-grey-floor.jpg"
+    }
+]
 
 @app.route("/")
 def home():
